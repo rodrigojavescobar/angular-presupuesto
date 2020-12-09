57d2a63160d6a063aee8f069b9f3718f343c73e3
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-09 15:41:08 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ingresarPresupuesto', pathMatch: 'full' },
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent },
+  { path: 'gastos', component: GastosComponent },
+  { path: '**', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
