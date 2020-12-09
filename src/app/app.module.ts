cd0b0de249ceb803aef5cc5f3c1553cefa31ae78
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-09 16:34:45 -0300

    ingresar presupuesto TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
@@ -18,7 +22,8 @@ import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-ga
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
