36786497bf9e67c33fa45e7c0dcbe6afd973dfe2
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-09 15:15:53 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarGastoComponent } from './components/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
