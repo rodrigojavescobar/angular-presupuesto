ab55fe4d219bffb57744de4b6c8995b1a223e62e
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-10 10:18:42 -0300

    comunicacion entre componentes

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gasto',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarGastoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _presupuestoService: PresupuestoService) {
+    this._presupuestoService.getGastos().subscribe(data => {
+      console.log(data);
+    })
+   }
 
   ngOnInit(): void {
   }
