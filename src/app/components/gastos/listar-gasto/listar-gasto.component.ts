28ceb4d2bfdb097a7c80aecc5f21fede73bd5569
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-10 10:34:05 -0300

    Listadod de gatos HTML

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
@@ -6,15 +7,26 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnInit {
+export class ListarGastoComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
 
   constructor(private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGastos().subscribe(data => {
+    this.presupuesto = 0;
+    this.restante = 0;
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data);
     })
    }
 
   ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
